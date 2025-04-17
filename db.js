7914eed2a198de29aa4eae80a7c7f76dcee6869d
princesoni6890 <sarrafprince37@gmail.com>
2025-04-17 10:58:51 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // MongoDB connection URL
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels'; // localhost ke jagah 127.0.0.1 best practice hai
+//const mongoURL = process.env.MONGODB_URL_LOCAL// localhost ke jagah 127.0.0.1 best practice hai
 
+const mongoURL = process.env.MONGODB_URL;
 // Connect to MongoDB
+
+
 mongoose.connect(mongoURL)
   .then(() => {
     console.log('Connected to MongoDB server');
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 
 const db = require('./db');
+require('dotenv').config();
 const person = require('./modelss/person');
 
 
@@ -9,7 +10,7 @@ const person = require('./modelss/person');
 
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
-
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -23,6 +24,8 @@ const personRoutes = require('./routes/personRoutes');
 const menuItemsRoutes = require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemsRoutes);
+
+
 
 app.listen(3000, ()=>{
     console.log('listening on port');
